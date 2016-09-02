d1e535202f12411bb954e6aba879edf149629aff
aenukond <aenukond@LH7U0CNU350965J.ms.ds.uhc.com>
2016-09-02 16:17:44 +0530

    yyyy

--- src/gitInitiativeSaiSree/FirstGitClass.java
+++ src/gitInitiativeSaiSree/FirstGitClass.java
@@ -19,7 +19,7 @@ public class FirstGitClass {
 		int c= a+b;
 		System.out.println("Addition of a and b is:: " + c);
 		System.out.println("Addition of a and bUUUU is:: " + c);
-		System.out.println("Addition of a and biiiiiiiU is:: " + c);
+		System.out.println("Addition of a and biiiiiiiUyyyyyyy is:: " + c);
 		
 	}
 
