8a0fcde2c54215f9c5b09e208245bc5dc50524e3
sgangula <sgangula@LH7U0CNU40999VZ.ms.ds.uhc.com>
2016-09-02 15:28:42 +0530

    second file commit

---
+++ src/gitInitiativeSaiSree/SecondGitClass.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package gitInitiativeSaiSree;
+
+/**
+ * @author sgangula
+ *
+ */
+public class SecondGitClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=13;
+		int b=4;
+		int c= a-b;
+		System.out.println("Difference of a and b is:: " + c);
+	}
+}
