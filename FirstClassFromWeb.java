085595e6a1ae706753963f68db6157745ce03264
saisreeG <saisree_gangula@optum.com>
2016-09-06 11:38:47 +0530

    Create FirstClassFromWeb.java

---
+++ FirstClassFromWeb.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package gitInitiativeSaiSree;
+
+/**
+ * @author sgangula
+ *
+ */
+public class FirstClassFromWeb {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+		int b=4;
+		int c= a+b;
+		System.out.println("Addition of a and b is:: " + c);
+	}
+
+}
